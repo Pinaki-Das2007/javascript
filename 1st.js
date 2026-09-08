f5941c3515b7a08fbe27428ab4459bb116326b94
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-08 12:29:22 +0530

    js concept clear

--- 1st.js
+++ 1st.js
@@ -1,2 +1,28 @@
-console.log("Hello, Pinaki is here.");
-alert("Hello, Pinaki is here.");
+let name = "tony stark";
+let age ;
+
+const pi = 3.14;
+let y = BigInt("1234567890123456789012345678901234567890");
+
+
+console.log(age);
+console.log(name);
+{
+    let a = 10;
+    console.log(a);
+} 
+
+
+// object
+
+const student = {
+    fullName : "Rahul Kumar",
+    age : 20,
+    cgpa : "8.5",
+    isPass : true
+};
+
+
+student["age"] +=1;
+
+console.log(student.age);
